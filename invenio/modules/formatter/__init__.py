73a12f31cbd6e44161a416329a22787d9db5a970
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2014-08-05 13:34:36 +0200

    formatter: xm hidden tags fix
    * Fixes issue where hidden tags was not filtered out in xm format due
      to that user info was not passed to the format_record function.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- invenio/modules/formatter/__init__.py
+++ invenio/modules/formatter/__init__.py
@@ -537,7 +537,6 @@ def print_records(recIDs, of='hb', ln=None, verbose=0,
     from flask import request
     from invenio.base.i18n import wash_language
     from invenio.ext.template import render_template_to_string
-    from invenio.modules.formatter.engine import format_record
     from invenio.modules.search.models import Format
     from invenio.utils.pagination import Pagination
     from invenio.modules.formatter.engine import TEMPLATE_CONTEXT_FUNCTIONS_CACHE
